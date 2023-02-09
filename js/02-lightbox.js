0b50669b5af28344703161c441d00732249af891
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-02-09 10:30:34 +0200

    Add corrections to the mentor's comments

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,16 +9,15 @@ const markup = galleryItems
   .map(({ preview, original, description }) => {
     return `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
-</a>;
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
 </li>`;
   })
   .join("");
 
 galleryContainer.innerHTML = markup;
 
-(function () {
-  var $gallery = new SimpleLightbox(".gallery a", {
-    captionDelay: 250,
-  });
-})();
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
